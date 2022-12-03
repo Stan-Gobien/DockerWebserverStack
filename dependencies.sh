45e7100ce5bea9dc27e82fd9674353f19c8cb1b0
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-12-03 21:43:16 +0100

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,16 @@
+#DNS Nameserver
+echo "Create a resolv.conf file"
+echo nameserver 1.1.1.1 > /etc/resolv.conf
+
+#Packages
+echo "Install packages"
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y ssmtp
+
+#PHP extensions
+echo "Install php extension"
+
+curl -o /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions --no-progress-meter
+IPE_GD_WITHOUTAVIF=1 && chmod +x /usr/local/bin/install-php-extensions && install-php-extensions gd gettext mysqli apcu exif mcrypt zip
+
+echo "Create file to let startup know entrypoint is finished"
+touch /var/.dependenciesfinished
