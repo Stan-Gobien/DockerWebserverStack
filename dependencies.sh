63a9a46ff2ecb324a95cee8066924c1052d2a7ab
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-12-04 00:06:20 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,19 +4,22 @@ echo nameserver 1.1.1.1 > /etc/resolv.conf
 
 #Packages
 echo "Install packages"
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y ssmtp inetutils-ping wget
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y ssmtp inetutils-ping wget git
 
 #Overwrite ssmtp.conf file
 mv /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.orig
 mv /etc/ssmtp/ssmtp.conf.custom /etc/ssmtp/ssmtp.conf
+
+sleep 10
 
 #PHP extensions
 echo "Install php extension"
 
 wget -O /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions
 chmod +x /usr/local/bin/install-php-extensions 
-IPE_GD_WITHOUTAVIF=1 && install-php-extensions gd gettext mysqli apcu exif mcrypt zip
-
+install-php-extensionsgettext mysqli apcu exif mcrypt zip
+install-php-extensionsgettext mysqli apcu exif mcrypt zip
+IPE_GD_WITHOUTAVIF=1 && install-php-extensions gd
 
 echo "Create file to let entrypoint know dependencies.sh has run"
 touch /var/.dependenciesfinished
