9a4801513aebdf538f90e459d4068cf907ecbccf
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-12-03 21:56:40 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -12,5 +12,8 @@ echo "Install php extension"
 curl -o /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions --no-progress-meter
 IPE_GD_WITHOUTAVIF=1 && chmod +x /usr/local/bin/install-php-extensions && install-php-extensions gd gettext mysqli apcu exif mcrypt zip
 
+docker-php-ext-install mysqli
+docker-php-ext-enable mysqli
+
 echo "Create file to let startup know entrypoint is finished"
 touch /var/.dependenciesfinished
