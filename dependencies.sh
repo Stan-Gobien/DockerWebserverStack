00a949eae97e98df671a01cdec2a336829b0c63a
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-12-03 23:57:28 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -2,15 +2,21 @@
 echo "Create a resolv.conf file"
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 
+#Packages
+echo "Install packages"
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y ssmtp inetutils-ping wget
+
+#Overwrite ssmtp.conf file
+mv /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.orig
+mv /etc/ssmtp/ssmtp.conf.custom /etc/ssmtp/ssmtp.conf
+
 #PHP extensions
 echo "Install php extension"
 
-curl -o /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions --no-progress-meter
-IPE_GD_WITHOUTAVIF=1 && chmod +x /usr/local/bin/install-php-extensions && install-php-extensions gd gettext mysqli apcu exif mcrypt zip
+wget -O /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions
+chmod +x /usr/local/bin/install-php-extensions 
+IPE_GD_WITHOUTAVIF=1 && install-php-extensions gd gettext mysqli apcu exif mcrypt zip
 
-#Packages
-echo "Install packages"
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y ssmtp
 
-echo "Create file to let startup know entrypoint is finished"
+echo "Create file to let entrypoint know dependencies.sh has run"
 touch /var/.dependenciesfinished
