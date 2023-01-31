08262baf9357d1d4cc8639af91df86bd567a938e
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2023-01-31 21:07:07 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo Entrypoint.sh start
+set -e
+
+echo Update script files from Github
+#You can disable this for security reasons or use your own repo with script files
+
+curl -o  /bin/entrypoint.sh https://raw.githubusercontent.com/Stan-Gobien/DockerWebserverStack/cloud01/entrypoint.sh --no-progress-meter
+curl -o  /bin/dependencies.sh https://raw.githubusercontent.com/Stan-Gobien/DockerWebserverStack/cloud01/dependencies.sh --no-progress-meter
+chmod +x /bin/entrypoint.sh && chmod +x /bin/dependencies.sh
+
+## Running passed command via exec
+if [[ "$1" ]]; 
+then
+        eval "$@"
+else
+        sleep 5
+        FILE=/var/.dependenciesfinished
+        if [ -f "$FILE" ]
+        then
+            echo "Dependencies already installed. $FILE exists."
+        else
+            echo "Running dependencies installation via dependencies.sh"
+            /bin/dependencies.sh
+        fi
+        sleep 5
+fi
+php-fpm
